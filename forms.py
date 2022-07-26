3453281e8346dcf91c5bc4fc68b8351f500e99eb
faso-dev <jeromeonadja28@gmail.com>
2022-07-26 10:07:18 +0000

    feat(out): remove unused import

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, URL, NumberRange, Regexp
+from wtforms.validators import DataRequired, URL, Regexp
 
 
 class ShowForm(Form):
